b9da3867e2a011927fecee35f916027de466b6d2
schramax <schrammemaxime@gmail.com>
2026-04-02 21:09:23 +0200

    Uploaded app files
    Upload of app files

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'eggs-sharer-v3';
+const ASSETS = [
+  'EggsSharer.html',
+  'manifest.json',
+  'EggsSharer.png',
+  'https://unpkg.com/lucide@latest',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => {
+        console.warn('Cache addAll failed', err);
+      });
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
